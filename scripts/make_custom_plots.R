c2d9ed9b8d3ede257053e50dee60a0566d5e49a0
Cat Pien <catarina.pien@gmail.com>
2025-03-03 10:42:38 -0800

    minor edit for looking at files

--- scripts/make_custom_plots.R
+++ scripts/make_custom_plots.R
@@ -1,15 +1,29 @@
 library(dplyr)
 library(ggplot2)
 library(here)
+library(tidyr)
 
-output <- readRDS(here("output/model_outputs/alt1_status-quo_output.rds"))
-lambda<-read.table(here("output/model_outputs/alt1_status-quo_lamAB.txt"),header=T)
-lambdamn<-read.table(here("output/model_outputs/alt1_status-quo_lamABmn.txt"),header=T)
+alt <-  "alt2_MaxDS_Even"
+
+output <- readRDS(here(paste0("output/model_outputs/", alt, "_output.rds")))
+lambda<-read.table(here(paste0("output/model_outputs/", alt, "_lamAB.txt")),header=T)
+lambdamn<-read.table(here(paste0("output/model_outputs/", alt, "_lamABmn.txt")),header=T)
+abundance <- read.csv(here(paste0("output/model_outputs/", alt, "_abundance.csv")))
 # defined in number 1 of get summaries
 
+# lambda file
 lambda_df <- as.data.frame(lambda) %>%
   cbind(data.frame(year = 1996:2014))
 
+# lambda mean file
+names <- data.frame(description = c("mean_all_years", "mean_2007_2014", "mean_2005_2014", "mean_1995_2006", "mean_AN_W", "mean_D_C", "mean_1997_2014", "L95CL", "U95CL")) 
+lam.mn.df <- data.frame(lam.mn = lam.mn)
+lammn_df <- cbind(names, lam.mn.df)
+
+ggplot(lammn_df) + geom_col(aes(x = description, y = lam.mn)) +
+  theme(axis.text.x = element_text(angle  = 90))
+
+# plot lambda
 ggplot(lambda_df) +
   geom_line(aes(year, mean), color = "magenta4") +
   geom_line(aes(year, min), color = "magenta3", linetype = "dotted") +
