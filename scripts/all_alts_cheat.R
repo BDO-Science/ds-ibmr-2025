8f17e108e677f9cce514c724269e6bd5e50d8eb3
Cat Pien <catarina.pien@gmail.com>
2025-03-06 14:03:13 -0800

    saved all lambda outputs together

---
+++ scripts/all_alts_cheat.R
@@ -0,0 +1,46 @@
+# List of all the alternative filenames to run with get summaries script
+# Run one alt at a time to save 
+
+alt <- "alt1_StatusQuo_output_SMSCGfixed.rds"
+alt.name <- "alt1"
+action.name <- ('StatusQuo')
+
+alt <- "alt2_MaxDS_Even_SMSCGfixed.rds"
+alt.name <- "alt2"
+action.name<- ('MaxDS_Even')
+
+alt <- "alt4_MaxDS_Hist_SMSCGfixed.rds"
+alt.name <- "alt4"
+action.name<- ('MaxDS_Hist')
+
+alt <- "alt5_SummerFall_Even_SMSCGfixed.rds"
+alt.name <- "alt5"
+action.name<- ('SummerFall_Even')
+
+alt <- "alt6_Summer_Even_output_SMSCGfixed.rds"
+alt.name <- "alt6"
+action.name<- ('Summer_Even')
+
+alt <- "alt7_Summer_Even_AltSMSCG_SMSCGfixed.rds"
+alt.name <- "alt7"
+action.name<- ('Summer_Even_AltSMSCG')
+
+alt <- "alt8_SummerFall_Hist_SMSCGfixed.rds"
+alt.name <- "alt8"
+action.name<- ('SummerFall_Hist')
+
+alt <- "alt9_Summer_Hist_SMSCGfixed.rds"
+alt.name <- "alt9"
+action.name<- ('Summer_Hist')
+
+alt <- "alt10_June_SMSCGfixed.rds"
+alt.name <- "alt10"
+action.name<- ('June')
+
+alt <- "alt11_MaxWater_output_SMSCGfixed.rds"
+alt.name <- "alt11"
+action.name=c('MaxWater')
+
+alt <- "alt12_MaxWater_noSMSCG_SMSCGfixed.rds"
+alt.name <- "alt12"
+action.name<- ('MaxWater_noSMSCG')
---
+++ scripts/read_lambda_outputs.R
@@ -0,0 +1,66 @@
+##########
+####read_lambda_outputs.R####
+# Read in files of relevance to gather all lambda values in a data frame.
+# Created by Catarina Pien 
+
+library(stringr)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tidyr)
+
+output_path <- here::here("output/model_outputs/")
+
+# Read all files and filter to lambda mean files
+fp_lam <- dir(here(output_path), full.names = TRUE)
+fp_lam2 <- data.frame(fp_lam) %>%
+  filter(grepl("lamABmn", fp_lam))
+
+fp_lam_char <- as.character(fp_lam2$fp_lam)
+ls_lam <- map(fp_lam_char, read.table)
+
+# Tidy up the data into a data frame
+df_lam <- list_cbind(ls_lam)
+# altnums <- c("alt1", "alt10", "alt11", "alt12", "alt2", "alt4", "alt5", "alt6", "alt7", "alt8", "alt9")
+cols <- c("StatusQuo", "June", "MaxWater", "MaxWater_noSMSCG", "MaxDS_Even",
+          "MaxDS_Hist", "SummerFall_Even", "Summer_Even", "Summer_Even_AltSMSCG",
+          "SummerFall_Hist", "Summer_Hist")
+colnames(df_lam) <- cols
+names <- data.frame(description = c("mean_all_years", "mean_2007_2014", "mean_2005_2014", "mean_1995_2006", "mean_AN_W", "mean_D_C", "mean_1997_2014", "L95CL", "U95CL")) 
+lammn_df <- cbind(names, df_lam)
+
+# Write file
+# write_csv(lammn_df, here(output_path, "mean_lambda_all_alts.csv"))
+
+# Make a long version of the data for plotting
+long <- pivot_longer(lammn_df, cols = StatusQuo:Summer_Hist, names_to = "alt", values_to = "lambdaval") %>%
+  mutate(alt = as.factor(alt),
+           # alt = forcats::fct_relevel(alt,  c("alt1", "alt2", "alt4", "alt5", "alt6", "alt7", "alt8", "alt9", "alt10", "alt11", "alt12"))) %>%
+         alt = forcats::fct_relevel(alt,  c("StatusQuo", "MaxDS_Even","MaxDS_Hist", "SummerFall_Even", "Summer_Even", "Summer_Even_AltSMSCG",
+                                            "SummerFall_Hist", "Summer_Hist", "June", "MaxWater", "MaxWater_noSMSCG"))) 
+
+# write_csv(long, here(output_path, "mean_lambda_all_alts_long.csv"))
+
+# Plot lambda
+ggplot() + 
+  geom_point(data = long %>%
+               filter(description %in% c("mean_all_years", "mean_AN_W")), aes(x = alt, y = lambdaval), size = 2) +
+  geom_hline(yintercept = 1, linetype = "dashed")+
+  facet_wrap(~description, nrow = 2, scales = "free_y") + 
+  labs(y = "Mean Lambda")+ 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 0.99),
+        axis.title.x = element_blank())
+
+
+# Plot with confidence limits
+cls <- long %>% filter(description %in% c("mean_all_years", "L95CL", "U95CL")) %>%
+  pivot_wider(names_from = "description", values_from = "lambdaval")
+
+ggplot() +
+  geom_point(data = cls, aes(x = alt, y = mean_all_years), size = 3) +
+  geom_errorbar(data = cls, aes(x = alt, ymin = L95CL, ymax = U95CL ))+
+  labs(y = "Mean Lambda")+ 
+  theme_bw()+ 
+  theme(axis.text.x = element_text(angle = 90, hjust = 0.99),
+        axis.title.x = element_blank())
